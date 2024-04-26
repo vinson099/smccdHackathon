2570dab32cd1b3bd999b9e59281d16f9136b86d9
vinson099 <121653369+vinson099@users.noreply.github.com>
2024-04-25 17:59:01 -0700

    initialized the assets we need

--- sketch.js
+++ sketch.js
@@ -1,21 +1,34 @@
-let ball;
+let archer;
 let sprite2;
 let playing = true;
 let spriteImage;
 let tileImage;
 
 function preload(){
-	spriteImage = loadImage("assets\\catAstro.png");
-	wallImage = loadImage("assets\\Tiny Swords (Update 010)\\Terrain\\Ground\\Tilemap_Elevation.png");
+	sheet_archer = loadImage('assets/Tiny Swords (Update 010)/Factions/Knights/Troops/Archer/Archer + Bow/Archer_Blue_(NoArms).png');
+	sheet_wall = loadImage('assets/Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation.png');
+	sheet_ground = loadImage('assets/Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Flat.png');
+	water_img = loadImage('assets/Tiny Swords (Update 010)/Terrain/Water/Water.png')
+	sheet_foam = loadImage('assets/Tiny Swords (Update 010)/Terrain/Water/Foam/Foam.png');
 }
 
 function setup() {
 	new Canvas(1920, 1080, 'fullscreen');
 
-	ball = new Sprite();
-	ball.image	= spriteImage;
-	sprite2 = new Sprite();
-	sprite2.image = wallImage;
+	archer = new Sprite();
+	archer.spriteSheet = sheet_archer;
+	archer.addAni({ w: 192, h: 192, row: 0, col: 1})
+
+
+
+	// sprite2 = new Sprite();
+	// sprite2.image = sheet_wall.get(0, 256, 190, 125);
+	
+}
+
+function movement(){
+	// need to add movement animation 
+	// not sure if i should do on kb press or in a function
 }
 
 
@@ -26,12 +39,21 @@ function draw() {
 
 	}
 	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
+		archer.speed = 10;
+		archer.moveTo(mouse);
 	}
-	if (kb.pressing('w')) { ball.y -= 3 }
-	if (kb.pressing('a')) { ball.x -= 3 }
-	if (kb.pressing('s')) { ball.y += 3 }
-	if (kb.pressing('d')) { ball.x += 3 }
+	if (kb.pressing('w')) { 
+		archer.y -= 3 
+
+	}
+	if (kb.pressing('a')) { 
+		archer.x -= 3 
+	}
+	if (kb.pressing('s')) { 
+		archer.y += 3 
+	}
+	if (kb.pressing('d')) { 
+		archer.x += 3 
+	}
 	if (kb.pressing('esc')) {}
 }
